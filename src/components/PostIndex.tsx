40ee44f6678f09ecf78761425ccadf0b750e9f3e
Jonathan Hudak <hudak.jonathan@gmail.com>
2022-06-21 22:21:06 -0700

    Edit qr code post

--- src/components/PostIndex.tsx
+++ src/components/PostIndex.tsx
@@ -7,11 +7,13 @@ const LazyRelativePostDate = React.lazy(() => import("./RelativePostDate"));
 interface PostFrontMatter {
   date: string;
   title: string;
+  published: boolean;
 }
 
 interface Post {
   slug: string;
   frontmatter: PostFrontMatter;
+  fileAbsolutePath: string;
   excerpt: string;
 }
 
@@ -19,7 +21,11 @@ interface PostIndexProps {
   posts: Post[];
 }
 
-function PostDate({ dateTime }) {
+interface PostDateProps {
+  dateTime: string;
+}
+
+function PostDate({ dateTime }: PostDateProps) {
   const isSSR = typeof window === "undefined";
   return (
     <div className="flex flex-justify">
@@ -39,22 +45,87 @@ function PostDate({ dateTime }) {
   );
 }
 
+enum Filter {
+  Posts = "posts",
+  Published = "published",
+}
+
+const FILTERS = {
+  [Filter.Posts]: (n: Post) => n.fileAbsolutePath.includes("/posts/"),
+  [Filter.Published]: (n: Post) => {
+    return n.frontmatter.published === true;
+  },
+};
+
+function applyFilters(posts: Post[], filters: Filter[]): Post[] {
+  return filters.reduce(
+    (acc, filterKey) => acc.filter(FILTERS[filterKey]),
+    posts
+  );
+}
+
+interface AdminFilters {
+  currentFilters: Filter[];
+  onToggle: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+function AdminFilters({ currentFilters, onToggle }: AdminFilters) {
+  if (process.env.NODE_ENV === "development") {
+    return (
+      <div>
+        <hr />
+        <h2>Admin Section</h2>
+        <label>Show only published posts:</label>&nbsp;
+        <input
+          name="published"
+          type="checkbox"
+          onChange={onToggle}
+          checked={currentFilters.includes(Filter.Published)}
+        />
+      </div>
+    );
+  }
+
+  return null;
+}
+
 export default function PostsIndex({ posts }: PostIndexProps) {
+  const [filters, setFilters] = React.useState<Filter[]>([
+    Filter.Posts,
+    Filter.Published,
+  ]);
+
+  function onToggleFilter(e: React.ChangeEvent<HTMLInputElement>) {
+    const { name, checked } = e.target;
+    const filter = name as Filter;
+    const newFilters = checked
+      ? [...filters, filter]
+      : filters.filter((f) => f !== filter);
+    setFilters(newFilters);
+  }
+
+  console.log(posts);
+
+  console.info(filters, applyFilters(posts, filters));
+
   return (
-    <ul className="PostsIndex">
-      {posts.map((post: Post) => (
-        <li key={post.slug} className="u-flow">
-          <h2>
-            <Link to={`/${post.slug.replaceAll("_", "-")}`}>
-              {post.frontmatter.title}
-            </Link>
-          </h2>
-          <PostDate dateTime={post.frontmatter.date} />
-          <div>
-            <MDXProvider>{post.excerpt}</MDXProvider>
-          </div>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul className="PostsIndex">
+        {applyFilters(posts, filters).map((post: Post) => (
+          <li key={post.slug} className="u-flow">
+            <h2>
+              <Link to={`/${post.slug.replaceAll("_", "-")}`}>
+                {post.frontmatter.title}
+              </Link>
+            </h2>
+            <PostDate dateTime={post.frontmatter.date} />
+            <div>
+              <MDXProvider>{post.excerpt}</MDXProvider>
+            </div>
+          </li>
+        ))}
+      </ul>
+      <AdminFilters onToggle={onToggleFilter} currentFilters={filters} />
+    </>
   );
 }
